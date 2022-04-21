049b909bbaf2c0e01a4d20e6c3ef029c647cfae8
evinracher <eevinley@gmail.com>
2022-04-21 16:42:46 -0500

    7: Presentación y Estructura de Archivos de nuestro proyecto: Platziswag

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Platzi store`,
+    description: `A sample store with Gatsby`,
+    author: `evinracher`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
