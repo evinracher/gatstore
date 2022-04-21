03784156a1d0da9e8bf264ae4fd80ba4c83b1e8e
evinracher <eevinley@gmail.com>
2022-04-21 17:34:59 -0500

    9: Usando React para manejar la parte visual e interactiva de nuestra aplicación

---
+++ src/components/Jumbo.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { StyledJumbo } from "../styles/components"
+
+export default function Jumbo() {
+  return (
+    <StyledJumbo>
+      <div>
+        <h2>Get the best products!</h2>
+        <small>Good merchandising for best students</small>
+      </div>
+    </StyledJumbo>
+  )
+}
