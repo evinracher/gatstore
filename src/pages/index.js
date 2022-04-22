8696f0afa16e03fd7e4084f20549fb1bd4a8a7f1
evinracher <eevinley@gmail.com>
2022-04-22 12:11:39 -0500

    19: Estilizando nuestros componentes con styled-components

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link, graphql } from "gatsby";
 
 import { SEO, Jumbo } from "../components";
+import styled from "styled-components";
 
 export const query = graphql`
   query GET_DESCRIPTION {
@@ -17,6 +18,17 @@ export const query = graphql`
   }
 `;
 
+const Button = styled.button`
+  width: 6rem;
+  background-color: #98ca3f;
+  border: none;
+  border-radius: 10px;
+  color: ${props => props.color};
+  &:hover {
+    transform: scale(1.5);
+  }
+`;
+
 const IndexPage = ({ data }) => (
   <>
     <SEO title="Home" />
@@ -24,6 +36,7 @@ const IndexPage = ({ data }) => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <Button color="gray">Buy</Button>
     <Link to="/page-2/">Go to page 2</Link>
   </>
 );
