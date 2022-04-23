f9fed49c0fac47405e95426aafd11360297cb255
evinracher <eevinley@gmail.com>
2022-04-23 12:51:53 -0500

    27: StaticQuery vs. useStaticQuery

--- src/components/image.js
+++ src/components/image.js
@@ -1,22 +1,18 @@
 import React from "react";
 import Img from "gatsby-image";
-import { graphql, StaticQuery } from "gatsby";
+import { graphql, StaticQuery, useStaticQuery } from "gatsby";
 
 export default function Image({ name }) {
-  return (
-    <StaticQuery
-      query={graphql`
-        query GET_IMAGE {
-          icon: file(relativePath: { eq: "icon.png" }) {
-            childImageSharp {
-              fluid(maxWidth: 1000) {
-                ...GatsbyImageSharpFluid
-              }
-            }
+  const data = useStaticQuery(graphql`
+    query GET_IMAGE {
+      icon: file(relativePath: { eq: "icon.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 1000) {
+            ...GatsbyImageSharpFluid
           }
         }
-      `}
-      render={data => <Img fluid={data[name].childImageSharp.fluid} />}
-    ></StaticQuery>
-  );
+      }
+    }
+  `);
+  return <Img fluid={data[name].childImageSharp.fluid} />;
 }
